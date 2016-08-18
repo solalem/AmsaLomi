0bd7744438c8f5aa607769a051a0f37dea0e7f44
Solalem <soalem2001@hotmail.com>
2016-08-18 13:35:09 +0300

    Added mapping for DTO and model classes

--- AmsaLomi/Dto/Dto.cs
+++ AmsaLomi/Dto/Dto.cs
@@ -11,6 +11,22 @@ namespace AmsaLomi.Dto
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public static CountryProfile FromBusinessEntity(Models.CountryProfile entity)
+        {
+            return new CountryProfile { Id = entity.Id, Description = entity.Description, Name = entity.Name };
+        }
+
+        public Models.CountryProfile ToBusinessEntity()
+        {
+            return new Models.CountryProfile { Id = this.Id, Description = this.Description, Name = this.Name };
+        }
+
+        public static IEnumerable<CountryProfile> FromBusinessEntity(IEnumerable<Models.CountryProfile> list)
+        {
+            return (from s in list select CountryProfile.FromBusinessEntity(s)); ;
+            //return list.ConvertAll(new Converter<Models.CountryProfile, CountryProfile>(FromBusinessEntity));
+        }
     }
 
     public class RegionProfile
@@ -20,6 +36,22 @@ namespace AmsaLomi.Dto
         public string Description { get; set; }
         public int CountryProfileId { get; set; }
         public string CountryProfile { get; set; }
+
+        public static RegionProfile FromBusinessEntity(Models.RegionProfile entity)
+        {
+            return new RegionProfile { Id = entity.Id, Description = entity.Description, Name = entity.Name, CountryProfile = entity.CountryProfile.Name, CountryProfileId = entity.CountryProfileId };
+        }
+
+        public Models.RegionProfile ToBusinessEntity()
+        {
+            return new Models.RegionProfile { Id = this.Id, Description = this.Description, Name = this.Name, CountryProfileId = this.CountryProfileId };
+        }
+
+        public static IEnumerable<RegionProfile> FromBusinessEntity(IEnumerable<Models.RegionProfile> list)
+        {
+            return (from s in list select RegionProfile.FromBusinessEntity(s)); ;
+            //return list.ConvertAll(new Converter<Models.RegionProfile, RegionProfile>(FromBusinessEntity)); ;
+        }
     }
 
     public class ZoneProfile
@@ -29,6 +61,22 @@ namespace AmsaLomi.Dto
         public string Description { get; set; }
         public int RegionProfileId { get; set; }
         public string RegionProfile { get; set; }
+
+        public static ZoneProfile FromBusinessEntity(Models.ZoneProfile entity)
+        {
+            return new ZoneProfile { Id = entity.Id, Description = entity.Description, Name = entity.Name, RegionProfile = entity.RegionProfile.Name, RegionProfileId = entity.RegionProfileId };
+        }
+
+        public Models.ZoneProfile ToBusinessEntity()
+        {
+            return new Models.ZoneProfile { Id = this.Id, Description = this.Description, Name = this.Name, RegionProfileId = this.RegionProfileId };
+        }
+
+        public static IEnumerable<ZoneProfile> FromBusinessEntity(IEnumerable<Models.ZoneProfile> list)
+        {
+            return (from s in list select ZoneProfile.FromBusinessEntity(s)); ;
+            //return list.ConvertAll(new Converter<Models.ZoneProfile, ZoneProfile>(FromBusinessEntity)); ;
+        }
     }
 
     public class WoredaProfile
@@ -39,6 +87,21 @@ namespace AmsaLomi.Dto
         public int ZoneProfileId { get; set; }
         public string ZoneProfile { get; set; }
 
+        public static WoredaProfile FromBusinessEntity(Models.WoredaProfile entity)
+        {
+            return new WoredaProfile { Id = entity.Id, Description = entity.Description, Name = entity.Name, ZoneProfile = entity.ZoneProfile.Name, ZoneProfileId = entity.ZoneProfileId };
+        }
+
+        public Models.WoredaProfile ToBusinessEntity()
+        {
+            return new Models.WoredaProfile { Id = this.Id, Description = this.Description, Name = this.Name, ZoneProfileId = this.ZoneProfileId };
+        }
+
+        public static IEnumerable<WoredaProfile> FromBusinessEntity(IEnumerable<Models.WoredaProfile> list)
+        {
+            return (from s in list select WoredaProfile.FromBusinessEntity(s)); ;
+            //return list.ConvertAll(new Converter<Models.WoredaProfile, WoredaProfile>(FromBusinessEntity)); ;
+        }
     }
 
     public class Mahiber
@@ -48,18 +111,46 @@ namespace AmsaLomi.Dto
         public string Description { get; set; }
         public int WoredaProfileId { get; set; }
         public string WoredaProfile { get; set; }
+
+        public static Mahiber FromBusinessEntity(Models.Mahiber entity)
+        {
+            return new Mahiber { Id = entity.Id, Description = entity.Description, Name = entity.Name, WoredaProfile = entity.WoredaProfile.Name, WoredaProfileId = entity.WoredaProfileId };
+        }
+
+        public Models.Mahiber ToBusinessEntity()
+        {
+            return new Models.Mahiber { Id = this.Id, Description = this.Description, Name = this.Name, WoredaProfileId = this.WoredaProfileId };
+        }
+
+        public static IEnumerable<Mahiber> FromBusinessEntity(IEnumerable<Models.Mahiber> list)
+        {
+            return (from s in list select Mahiber.FromBusinessEntity(s)); ;
+            //return list.ConvertAll(new Converter<Models.Mahiber, Mahiber>(FromBusinessEntity)); ;
+        }
     }
 
-    public enum PaymentTypes
-    {
-        SMS, BankAccount, MobileBanking
-    }
     public class Payment
     {
         public int Id { get; set; }
-        public PaymentTypes Type { get; set; }
+        public Models.PaymentTypes Type { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public static Payment FromBusinessEntity(Models.Payment entity)
+        {
+            return new Payment { Id = entity.Id, Description = entity.Description, Name = entity.Name, Type=entity.Type };
+        }
+
+        public Models.Payment ToBusinessEntity()
+        {
+            return new Models.Payment { Id = this.Id, Description = this.Description, Name = this.Name };
+        }
+
+        public static IEnumerable<Payment> FromBusinessEntity(IEnumerable<Models.Payment> list)
+        {
+            return (from s in list select Payment.FromBusinessEntity(s)); ;
+            //return list.ConvertAll(new Converter<Models.Payment, Payment>(FromBusinessEntity)); ;
+        }
     }
 
     public class MahiberPayment
@@ -71,18 +162,46 @@ namespace AmsaLomi.Dto
         public string Mahiber { get; set; }
         public string Payment { get; set; }
         public int PaymentNumber { get; set; }
+
+        public static MahiberPayment FromBusinessEntity(Models.MahiberPayment entity)
+        {
+            return new MahiberPayment { Id = entity.Id, Description = entity.Description, Mahiber = entity.Mahiber.Name, MahiberId = entity.MahiberId, Payment = entity.Payment.Name, PaymentId=entity.PaymentId, PaymentNumber = entity.PaymentNumber };
+        }
+
+        public Models.MahiberPayment ToBusinessEntity()
+        {
+            return new Models.MahiberPayment { Id = this.Id, Description = this.Description, MahiberId = this.MahiberId, PaymentId = this.PaymentId, PaymentNumber = this.PaymentNumber };
+        }
+
+        public static IEnumerable<MahiberPayment> FromBusinessEntity(IEnumerable<Models.MahiberPayment> list)
+        {
+            return (from s in list select MahiberPayment.FromBusinessEntity(s)); ;
+            //return list.ConvertAll(new Converter<Models.MahiberPayment, MahiberPayment>(FromBusinessEntity)); ;
+        }
     }
 
-    public enum CurrencyTypes
-    {
-        Birr, Dollar, Pound, Euro
-    }
     public class Donation
     {
         public int Id { get; set; }
         public int MahiberPaymentId { get; set; }
         public string MahiberPayment { get; set; }
-        public CurrencyTypes CurrencyType { get; set; }
+        public Models.CurrencyTypes CurrencyType { get; set; }
         public int Amount { get; set; }
+
+        public static Donation FromBusinessEntity(Models.Donation entity)
+        {
+            return new Donation { Id = entity.Id, Amount = entity.Amount, CurrencyType = entity.CurrencyType, MahiberPayment= entity.MahiberPayment.Description, MahiberPaymentId = entity.MahiberPaymentId };
+        }
+
+        public Models.Donation ToBusinessEntity()
+        {
+            return new Models.Donation { Id = this.Id, Amount = this.Amount, CurrencyType = this.CurrencyType, MahiberPaymentId = this.MahiberPaymentId };
+        }
+
+        public static IEnumerable<Donation> FromBusinessEntity(IEnumerable<Models.Donation> list)
+        {
+            return (from s in list select Donation.FromBusinessEntity(s)); ;
+            //return list.ConvertAll(new Converter<Models.Donation, Donation>(FromBusinessEntity)); ;
+        }
     }
 }
