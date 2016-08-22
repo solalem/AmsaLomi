f73c11b2f9b934283482b30a6daf2a3b7be79524
solalem <solalem2001@gmail.com>
2016-08-22 10:01:37 +0300

    Added AspNetUser table in context

--- AmsaLomi/Models/AmsaLomiModels.cs
+++ AmsaLomi/Models/AmsaLomiModels.cs
@@ -98,4 +98,30 @@ namespace AmsaLomi.Models
         public virtual MahiberPayment MahiberPayment { get; set; }
     }
 
+    public class MahiberMember
+    {
+        public int Id { get; set; }
+        public int MahiberId { get; set; }
+        public string MemberId { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public virtual Mahiber Mahiber { get; set; }
+        public virtual AspNetUser Member { get; set; }
+    }
+
+    public class AspNetUser
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string PasswordHash { get; set; }
+        public string SecurityStamp { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public Nullable<System.DateTime> LockoutEndDateUtc { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public int AccessFailedCount { get; set; }
+        public string UserName { get; set; }
+    }
 }
